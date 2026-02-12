5005891514c54762fb442b4b78e0a20a1bc594d8
Kaytster <ktsharps@gmail.com>
2026-02-12 13:04:16 +0000

    test page for testing connection

---
+++ Client/src/pages/customerDashboard.jsx
@@ -0,0 +1,8 @@
+export default function CustomerDashboard() {
+return (
+    <div>
+        <h1>Customer Dashboard</h1> 
+        <p>Welcome to your dashboard! Here you can view your orders, manage your account, and more.</p> 
+        </div> 
+        );
+};
--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa65fc35941f611c3a46c20b027f87dad41f91d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6026fe711f0f910d18adc43e583e37226049e45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
