1e480389719948412873ad59809991636011a3a2
Kaytster <ktsharps@gmail.com>
2026-02-12 13:23:12 +0000

    test page

--- Client/src/pages/customerDashboard.jsx
+++ Client/src/pages/customerDashboard.jsx
@@ -3,6 +3,7 @@ return (
     <div>
         <h1>Customer Dashboard</h1> 
         <p>Welcome to your dashboard! Here you can view your orders, manage your account, and more.</p> 
+        <h3>Username Here</h3>
         </div> 
         );
 };
