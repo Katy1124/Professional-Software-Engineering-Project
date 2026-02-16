e2a9204fad0d03626bb6c5db50a93954558d45ec
Kaytster <ktsharps@gmail.com>
2026-02-16 09:43:16 +0000

    navbar

--- Client/src/pages/adminDashboard.jsx
+++ Client/src/pages/adminDashboard.jsx
@@ -1,6 +1,16 @@
 export default function AdminDashboard() {
   return (
     <div className="admin-dashboard">
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+            <button dta-mdb-button-init className="navbar-toggler" type="button" data-mdb-collapse-init
+            data-mdb-target="#navbarToggleExternalContent2" aria-controls="navbarToggleExternalContent2" aria-expanded="false"
+            aria-label="Toggle navigation">
+                <i className="fas fa-bars"></i>
+            </button>
+        </div>
+      </nav>
+
       <h1>Admin Dashboard</h1>
       <p>Welcome to the admin dashboard! Here you can manage users, view reports, and configure settings.</p>
     </div>
