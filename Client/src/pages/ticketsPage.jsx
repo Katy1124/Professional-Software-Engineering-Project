7fdd649bff7d8a1fc7d43abb0fd7658538c21365
Kaytster <ktsharps@gmail.com>
2026-02-16 13:16:15 +0000

    created page

---
+++ Client/src/pages/ticketsPage.jsx
@@ -0,0 +1,14 @@
+export default function TicketsPage() {
+  return (
+    <div className="tickets-page">
+      <nav className="navbar custom-nav">
+        <div className="container-fluid">
+          <a classNamen="navbar-brand" href="#">
+            <img src={giacomLogo} alt="GIACOM" width="110" height="24"></img></a>
+          <a className="navbar custom-link" href="#">Customer Dash</a>
+          <a className="navbar custom-link" href="#">Admin Dash</a>
+        </div>
+      </nav>
+      </div>
+  )
+}
