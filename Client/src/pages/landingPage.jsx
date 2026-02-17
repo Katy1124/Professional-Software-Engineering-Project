72d4fed0084c54988293562bb8eb267fbc8a8dd3
Kaytster <ktsharps@gmail.com>
2026-02-17 12:40:00 +0000

    page and css setup

---
+++ Client/src/pages/landingPage.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+import giacomLogo from '../assets/giacom-master-white-logo-1.png'; 
+import './landingPage.css';
+export default function LandingPage() { 
+    return (
+        <div className="landing-page">
+            
+        </div>
+     )
+}
