fae7d306e11e200302a10f220e2dfc4e67614cd7
Kaytster <ktsharps@gmail.com>
2026-02-17 12:24:40 +0000

    update

--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b940133edc15601db6aa98e88a48cea27eac61b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e032e8947da210dff1c39a1a2b360f6baa212ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
