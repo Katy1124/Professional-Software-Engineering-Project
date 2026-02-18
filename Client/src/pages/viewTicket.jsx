64aa199b70d284d00a3065a32d7950d0054bb18b
Kaytster <ktsharps@gmail.com>
2026-02-18 15:08:42 +0000

    changed formatting

--- Client/src/pages/viewTicket.jsx
+++ Client/src/pages/viewTicket.jsx
@@ -25,13 +25,54 @@ export default function ViewTicket() {
                 <div className="card-body">
                   <p style={{fontSize: '40px', fontWeight: 'bold'}}><span style={{paddingRight: '100px'}}>Ticket 108 </span><span style={{paddingLeft: '100px'}}>Sent by: User15645</span></p>
                   <p style={{fontSize: '25px', textAlign: 'left', paddingLeft: '100px', fontWeight: 'bold'}}>01/02/2026</p>
-                  <p style={{fontSize: '25px', fontWeight: 'bold'}}><span>Title: </span><span>Sign-up button not working</span></p>
-                  <div className="container">
-                    <p><span>Ticket Type: </span><span>Incident</span></p>
-                    <p><span>Severity: </span><span>Medium </span> <span>Business Impact: </span><span>Medium</span></p>
-                    <p><span>Users Affected: </span><span>~100 </span><span>Resolution Date: </span><span>04/02/2026</span></p>
-                    <p><span>Status: </span><span style={{padding: '5px', borderRadius: '5px', backgroundColor: '#236A49', color: 'white'}}>Active</span></p>
+                  
+                  <div className="container information">
+                    <div className='row title' style={{fontSize: '25px', fontWeight: 'bold'}}>
+                        <div className='col'>
+                            Title:  
+                        </div>
+                        <div className='col'>   
+                            Sign-up button not working
+                        </div> 
+                    </div>
+                    
+                    <div className='row type'>
+                        <div className='col'  style={{width: '10px'}}>
+                            <p>Ticket Type:</p>
+                        </div>
+                        <div className='col' >
+                            <p>Incident</p>
+                        </div> 
+                    </div>
+                    
+                    <div className='row severity'>
+                        <div className='col'>
+                            <p>Severity: Medium</p>
+                        </div>
+                        <div className='col'>
+                            <p>Business Impact: Medium</p>
+                        </div>
+                    </div>
+                    
+                    <div className='row estimate'>
+                        <div className='col'>
+                            <p>Users Affected: ~100</p>
+                        </div>
+                        <div className='col'>
+                            <p>Resolution Date: 04/02/2026</p>
+                        </div>
+                    </div>
+                    
+                    <div className='row desc'> 
+                        <div className='col'>
+                            <p>Description:</p>
+                        </div>
+                        <div className='col'>
+                            <p>Attachments:</p>
+                        </div>
+                    </div>
                   </div>
+
                 </div>
               </div>
           </div>
