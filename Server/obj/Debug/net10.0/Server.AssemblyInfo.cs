f899cdfff4a33eff3777688ab1792e374c6902f5
Kian Blades <kian.blades@student.shu.ac.uk>
2026-02-20 12:57:32 +0000

    Revert "Stop tracking build folders"
    This reverts commit 0c5801733cc513d654e68841f50b83561f0c4192.

--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b5120f282d0744ce4ff6b0ef355385425e67b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66dabd5f7f6a01f7aa9591d38feb02b811ec7a98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
