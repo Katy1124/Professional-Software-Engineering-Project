fae92db79af8af1d2b68b274f2225b3267021d79
Kian Blades <kian.blades@student.shu.ac.uk>
2026-02-20 10:06:31 +0000

    random change

--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d9c05c1aea0d628cbd0863c1e546bda9b96500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cc45c126a4eafae887a4d65a67e8f8a1541f7ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
