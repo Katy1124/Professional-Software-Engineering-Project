277e7c5600adc80bed897b64fdb741dda822400a
Kaytster <ktsharps@gmail.com>
2026-02-20 09:29:34 +0000

    setup navbar component

---
+++ Client/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import giacomLogo from '../assets/giacom-master-white-logo-1.png'; 
+import './navbar.css';
+
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            
+        </nav>
+    )
+}
