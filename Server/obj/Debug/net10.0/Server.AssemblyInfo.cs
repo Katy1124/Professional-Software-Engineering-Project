a54cba258f45e422ad7c4d5f05f08f54b0fa5210
Kaytster <ktsharps@gmail.com>
2026-03-06 10:07:57 +0000

    added logic to connect to API and show ticket status on dashboard. Had to use the severity as we dont have status yet

--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ba98a28e7d2fcd5a29b59cc977b699af36a903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db05b3982a656edffc981ccbfed5f329464c0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
