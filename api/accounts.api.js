11d3410ecf6dc50eeda64dd7269cf3c36b0f23ff
Kaytster <ktsharps@gmail.com>
2026-03-23 15:02:14 +0000

    updated

--- api/accounts.api.js
+++
@@ -1,9 +0,0 @@
-import { http } from "./http";
-
-export const accountsApi = {
-  list: () => http.get("/api/Accounts"),
-  getById: (id) => http.get(`/api/Accounts/${id}`),
-  create: (payload) => http.post("/api/Accounts", payload),
-  update: (id, payload) => http.put(`/api/Accounts/${id}`, payload),
-  remove: (id) => http.del(`/api/Accounts/${id}`),
-};
