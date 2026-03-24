fa3a56e1c9fadcfa59970e1acf9faa6f7c2e253b
Kaytster <ktsharps@gmail.com>
2026-03-24 10:15:22 +0000

    updated imports

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -11,9 +11,6 @@ import QuoteGenerator from './pages/quoteGenerator';
 import ViewTicket from './pages/viewTicket';
 import LoginPage from './pages/loginPage';
 // Temporary pages
-import TestTicket from './pages/viewTicket2';
-import TestQuoteCustomer from './pages/customerQuote2';
-import TestQuoteAdmin from './pages/quoteGenerator2';
 import CustQuotePage from './pages/customerQuotesPage';
 import AdminQuotePage from './pages/adminQuotesPage';
 
@@ -27,14 +24,14 @@ function App() {
         <Route path="/customer" element={<SecurePath allowedType={0}> <CustomerDashboard /> </SecurePath>} />
         <Route path="/ticketForm" element={<SecurePath allowedType={0}> <TicketForm /> </SecurePath>} />
         <Route path="/ticketsPage" element={<TicketsPage />} />
-        <Route path="/customerQuote" element={<CustomerQuote/>} />
+        <Route path="/customerQuote/:id" element={<CustomerQuote/>} />
         <Route path="/adminQuote" element={<SecurePath allowedType={1}> <QuoteGenerator /> </SecurePath>} />
         <Route path="/viewTicket" element={<ViewTicket />} />
         <Route path="/login" element={<LoginPage />} />
         {/* Temporary pages */}
-        <Route path="/viewTestTicket/:id" element={<TestTicket />}/>
+        {/* <Route path="/viewTestTicket/:id" element={<TestTicket />}/>
         <Route path="/viewQuoteAdmin/:id" element={<TestQuoteAdmin/>}/>
-        <Route path="/viewQuoteCustomer/:id" element={<TestQuoteCustomer/>}/>
+        <Route path="/viewQuoteCustomer/:id" element={<TestQuoteCustomer/>}/> */}
         <Route path="/custQuotepage/:id" element={<CustQuotePage/>}/>
         <Route path="/adminQuotePage" element={<AdminQuotePage/>}/>
       </Routes>
@@ -43,39 +40,3 @@ function App() {
 }
 
 export default App;
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
