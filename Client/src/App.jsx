6b10f85e90f0a0253412529288884189062c06b5
Kaytster <ktsharps@gmail.com>
2026-03-25 10:37:48 +0000

    updated app.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,14 +10,13 @@ import QuoteEstimate from './pages/quoteGenerator';
 // Customer Pages
 import CustomerDashboard from './pages/customerDashboard';
 import TicketForm from './pages/ticketForm';
-import AdminDashboard from './pages/adminDashboard';
+
 import TicketsPageAd from './pages/ticketsPageAd';
 import TicketsPageCus from './pages/ticketsPageCus';
 import CustomerQuotesPage from './pages/customerQuotesPage';
 import CustomerQuote from './pages/customerQuote';
 import TicketsPage from './pages/ticketsPage';
 import ViewTicket from './pages/viewTicket';
-import LoginPage from './pages/loginPage';
 // Temporary pages
 import TestTicket from './pages/viewTicketAd';
 import TestTicketcus from './pages/viewTicketCus';
