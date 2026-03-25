8a72ae3aa5d2e28cd060e1695569a27b94346ee3
Kian Blades <kian.blades@student.shu.ac.uk>
2026-03-25 14:21:18 +0000

    node modules

--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54d4d335c8f60a87e533e6c7619fb5bafa9ef7df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217db4cc81647475d75f907f79bb492b1d2a2324")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
